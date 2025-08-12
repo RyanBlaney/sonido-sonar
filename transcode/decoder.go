487ea7655a157a490947fc15ad7bf3790a174dce
Ryan Blaney <ryanblaney2004@gmail.com>
2025-08-12 16:08:40 -0700

    tweaked the decoder

--- transcode/decoder.go
+++ transcode/decoder.go
@@ -278,32 +278,29 @@ func (d *Decoder) DecodeURL(url string, duration time.Duration, streamType strin
 	// Add stream-type specific flags
 	switch streamType {
 	case "icecast":
-		// Add reconnection flags for Icecast streams
 		args = append(args,
-			"-re",
 			"-reconnect", "1",
 			"-reconnect_at_eof", "1",
 			"-reconnect_streamed", "1",
-			"-reconnect_delay_max", "2",
-			"-fflags", "+genpts+igndts+flush_packets", // Generate PTS, ignore DTS for live
-			"-rw_timeout", "30000000", // 30 second read timeout
-			"-timeout", "60000000", // 60 second total timeout
+			"-reconnect_delay_max", "1",
+			"-fflags", "+genpts+igndts+flush_packets",
+			"-rw_timeout", "5000000", // 5 second read timeout
+			"-timeout", "15000000", // 15 second total timeout
 		)
 	case "hls":
-		// HLS live streaming optimizations
 		args = append(args,
-			"-fflags", "+genpts+igndts+flush_packets",
-			"-live_start_index", "-1", // Start 3 segments back from live edge for stability
-			"-probesize", "1000000", // 1MB - enough to detect streams reliably
-			"-analyzeduration", "2000000", // 2 seconds - balance speed vs reliability
-			// "-re",                     // Read input at native frame rate
-			"-rw_timeout", "30000000", // 30 second read timeout
+			"-fflags", "+genpts+igndts+flush_packets+discardcorrupt",
+			"-live_start_index", "0", // Start at live edge
+			"-probesize", "100000", // Minimal probe - 100KB
+			"-analyzeduration", "500000", // 0.5 seconds analysis
+			"-rw_timeout", "5000000", // 5 second read timeout
+			"-timeout", "15000000", // 15 second total timeout
 			"-reconnect", "1",
 			"-reconnect_at_eof", "1",
 			"-reconnect_streamed", "1",
-			"-timeout", "60000000", // 60 second timeout
-			"-reconnect_delay_max", "2",
-			"-avoid_negative_ts", "make_zero", // Normalize timestamps to start at 0
+			"-reconnect_delay_max", "1",
+			"-avoid_negative_ts", "make_zero",
+			"-hls_flags", "delete_segments", // Don't cache segments
 		)
 	default:
 		// For unknown stream types, log a warning but continue
